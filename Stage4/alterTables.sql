9d7e6a18f9e7813712f4220356edd80dadc372d8
Rivka Sorscher <144739411+Rivka9768@users.noreply.github.com>
2025-06-09 20:29:53 +0300

    Update alterTables.sql

--- Stage4/alterTables.sql
+++ Stage4/alterTables.sql
@@ -1,3 +1,5 @@
+--1
+
 CREATE TABLE Shifts (
   shiftId SERIAL PRIMARY KEY,
   branchId NUMERIC NOT NULL,
@@ -6,7 +8,7 @@ CREATE TABLE Shifts (
   FOREIGN KEY (branchId) REFERENCES Branches(branchId)
 );
 
-
+--2
 
 CREATE TABLE EmployeeShifts (
   employeeId NUMERIC NOT NULL,
@@ -15,3 +17,16 @@ CREATE TABLE EmployeeShifts (
   FOREIGN KEY (employeeId) REFERENCES Employee(employeeId),
   FOREIGN KEY (shiftId) REFERENCES Shifts(shiftId)
 );
+
+--3
+
+CREATE TABLE LogChanges (
+  logId SERIAL PRIMARY KEY,
+  tableName VARCHAR NOT NULL,
+  operation VARCHAR NOT NULL, -- INSERT, UPDATE, DELETE
+  changedBy VARCHAR DEFAULT CURRENT_USER,
+  changeTime TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  oldData JSONB,
+  newData JSONB
+);
+
